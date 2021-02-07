b93d79080042a692f50a13bf0e028034b608ae6d
Einar Johan Trøan Sømåen <somaen@scummvm.org>
2021-02-08 00:00:13 +0100

    TINSEL: Map DECINV2

--- engines/tinsel/tinlib.cpp
+++ engines/tinsel/tinlib.cpp
@@ -4195,6 +4195,10 @@ NoirMapping translateNoirLibCode(int libCode, int32 *pp) {
 		pp -= mapping.numArgs - 1;
 		debug(7, "%s(0x%08X, 0x%08X, 0x%08X, 0x%08X, 0x%08X, 0x%08X, 0x%08X, 0x%08X)", mapping.name, pp[0], pp[1], pp[2], pp[3], pp[4], pp[5], pp[6], pp[7]);
 		break;
+	case 44:
+		mapping = NoirMapping{"DECINV2", DECINV2, 8};
+		pp -= mapping.numArgs - 1;
+		debug(7, "%s(0x%08X, 0x%08X, 0x%08X, 0x%08X, 0x%08X, 0x%08X, 0x%08X, 0x%08X)", mapping.name, pp[0], pp[1], pp[2], pp[3], pp[4], pp[5], pp[6], pp[7]);
 		break;
 	case 45:
 		mapping = NoirMapping{"DECLARELANGUAGE", DECLARELANGUAGE, 3};
